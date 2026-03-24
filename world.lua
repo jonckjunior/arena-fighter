36da6db9ffb399dc58caa86d938777ea3660ed9a
Jonck <jonckjuniorr@gmail.com>
2026-03-23 20:45:02 -0400

    Adding annotation

--- world.lua
+++ world.lua
@@ -12,7 +12,7 @@ local STANDARD_GRAVITY = 600
 ---@field speed table<integer, {value: number}>
 ---@field facing table<integer, {dir: number}>
 ---@field solid table
----@field collider table<integer, {shape: "circle"|"rect", radius: number|nil, w: number|nil, h: number|nil}>
+---@field collider table<integer, {shape: "circle"|"rect", radius: number|nil, w: number|nil, h: number|nil, ox: integer, oy: integer}>
 ---@field gun table<integer, {cooldown: number, maxCooldown: number, damage: number, bulletSpeed: number, spread: number, bulletCount: integer, isReloading: boolean, reloadTime: number, reloadTimer: number, maxAmmo: integer, currentAmmo: integer}>
 ---@field bullet table<integer, {ownerId: integer, damage: number, graceFrames: integer}>
 ---@field lifetime table<integer, {ttl: number}>
